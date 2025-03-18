da74b77f0cfd688f192faf58fd8db5b3396488f3
gabrielatab <gtaboada@ufl.edu>
2025-03-17 22:00:45 -0400

    creating pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
 
 function App() {
-  return <Navbar/>
+  let component
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />
+      break
+    case "/login":
+      component = <Login />
+      break
+    case "/about":
+      component = <About />
+      break
+  }
+
+  return( 
+  <>
+  <Navbar  />
+  {component}
+  </>
+  )
 }
 
 export default App;
