5fea9ef3836140f74451373a4f4efc563ca888e7
gabrielatab <gtaboada@ufl.edu>
2025-03-27 15:09:47 -0400

    styling home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,14 @@ export default function Home() {
     return (
         <div className="home-container">
             <div className="flex-container">
-                <div className="column">
-                    <h1 style={{ color: "red", fontSize: "36px", fontWeight: "bold" }}>
+                <div className="column outfit-font ">
+                    <h1 style={{ color: "white", fontSize: "80px", fontWeight: "600" }}>
                         FridgeChef
                     </h1>
+                    <div style={{ color: "#EC6D53", fontSize: "22px", maxWidth: "100%" }}>
+                        <p style={{marginTop: "20px"}}>Save money, save food, save time.</p>
+                        <p> A smarter way to eat for college students</p>
+                    </div>
                 </div>
 
                 <div className="column">
