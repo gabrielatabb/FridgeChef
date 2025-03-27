dda1fdb2c729adce1a5901481486deba4d2db52a
gabrielatab <gtaboada@ufl.edu>
2025-03-27 17:50:45 -0400

    adding log in

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,12 @@
 export default function Login() {
-    return <h1>Login</h1>
+    return (
+        <div className="login-container">
+            <div className="center-box">
+                <h1 className="outfit-font" style={{ color: "#EC6D53", fontSize: "65px", fontWeight: "600" }}>
+                    FridgeChef
+                </h1>
+                <p>Log in to continue.</p>
+            </div>
+        </div>
+    );
 }
