665bbcd8871c3d24c3613a4f2e477b1eaf64ed19
gabrielatab <gtaboada@ufl.edu>
2025-03-27 13:56:33 -0400

    formating home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,26 @@
+import "../styles.css";
+
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <div className="home-container">
+            <div className="flex-container">
+                <div className="column">
+                    <h1 style={{ color: "red", fontSize: "36px", fontWeight: "bold" }}>
+                        FridgeChef
+                    </h1>
+                </div>
+
+                <div className="column">
+                    <p>This portal has been created to provide 
+                        well-thought and well-explained solutions 
+                        for selected questions.
+                    </p>
+                    <p>Our team includes Sandeep Jain, Vaibhav Bajpai, 
+                        Shikhar Goel, Dharmesh Singh, and Shubham 
+                        Baranwal, all passionate developers and engineers.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
